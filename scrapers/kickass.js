6dea4888a53cb0f03c7abe09f09457c5357dd8d5
Tejas_patil <tejasvp25@gmail.com>
2020-05-01 16:37:50 +0530

    kickass.js : Revamp scraping logic

--- scrapers/kickass.js
+++ scrapers/kickass.js
@@ -7,82 +7,56 @@ const BASE_URL = require("./constants").KICKASS_BASE_URL;
 async function getKickassSearchResult(search) {
     const response = await axios.get(BASE_URL + search);
     const $ = cheerio.load(response.data);
-    const odd_rows = $(".odd");
-    const even_rows = $(".even");
+    const odd_rows = $("tr.odd , tr.even");
     const jsonRes = [];
-    if (odd_rows.length > 0) {
-        odd_rows.each((i, element) => {
-            name = $(element).find(".cellMainLink").text().replace("\n", "");
-            // uploader_name = $(element).find(".plain").text().substring(2);
-            uploader_name = $(
-                "table > tbody > tr:nth-child(2) > td:nth-child(1) > div.torrentname > div > span > a"
-            ).text();
-            file_size = $(element).children().eq(1).text().replace("\n", "");
-            upload_date = $(element).children().eq(3).text().replace("\n", "");
-            //  Seeders
-            seeders = $(element).children().eq(4).text().replace("\n", "");
-            //  Leechers
-            leechers = $(element).children().eq(5).text().replace("\n", "");
-            url =
-                "https://kickasstorrents.to" +
-                $(element).find(".cellMainLink").attr("href");
-            jsonRes.push({
-                name: name,
-                torrent_url: url,
-                seeders: seeders,
-                leechers: leechers,
-                upload_date: upload_date,
-                size: file_size,
-                uploader: uploader_name,
-                magnet : "",
-                website: "Kickass",
-            });
+    odd_rows.each((i, element) => {
+        name = $(element).find(".cellMainLink").text().replace("\n", "");
+        uploader_name = $(element)
+            .find("td:nth-child(3)")
+            .text()
+            .replace("\n", "");
+        file_size = $(element).find("td:nth-child(2)").text().replace("\n", "");
+        upload_date = $(element)
+            .find("td:nth-child(4)")
+            .text()
+            .replace("\n", "");
+        //  Seeders
+        seeders = $(element).find("td:nth-child(5)").text().replace("\n", "");
+        //  Leechers
+        leechers = $(element).find("td:nth-child(6)").text().replace("\n", "");
+        url =
+            "https://kickasstorrents.to" +
+            $(element).find(".cellMainLink").attr("href");
+        jsonRes.push({
+            name: name,
+            torrent_url: url,
+            seeders: seeders,
+            leechers: leechers,
+            upload_date: upload_date,
+            size: file_size,
+            uploader: uploader_name,
+            magnet: "",
+            website: "Kickass",
         });
-    }
-    if (even_rows.length > 0) {
-        even_rows.each((i, element) => {
-            name = $(element).find(".cellMainLink").text().replace("\n", "");
-            uploader_name = $(element).find(".plain").text().substring(2);
-            file_size = $(element).children().eq(1).text().replace("\n", "");
-            upload_date = $(element).children().eq(3).text().replace("\n", "");
-            //  Seeders
-            seeders = $(element).children().eq(4).text().replace("\n", "");
-            //  Leechers
-            leechers = $(element).children().eq(5).text().replace("\n", "");
-            url =
-                "https://kickasstorrents.to" +
-                $(element).find(".cellMainLink").attr("href");
-            jsonRes.push({
-                name: name,
-                seeders: seeders,
-                leechers: leechers,
-                upload_date: upload_date,
-                size: file_size,
-                uploader: uploader_name,
-                url: url,
-                website: "Kickass",
-            });
-        });
-    }
+    });
+
     return jsonRes;
 }
 
-router.get("/kickassMagnet", async (req, res) => {
-    let url = req.query.url;
-    let response = await axios.get(url);
-    let $ = cheerio.load(response.data);
+router.get("/kickass_mg", async (req, res) => {
+    var url = req.query.url;
+    var response = await axios.get(url);
+    var $ = cheerio.load(response.data);
     magnet = $(".kaGiantButton ").attr("href");
-    res.send(magnet);
-    res.end();
+    res.status(200).json({"magnet":magnet}).end();
 });
 
 router.get("/kickass", async function (req, res) {
     console.log(BASE_URL);
     //  Get the String to be Searched from URL
-    let search = req.query.search;
+    var search = req.query.search;
     jsonResult = await getKickassSearchResult(search);
-    res.json({"data":jsonResult});
-    res.end();
+    res.status(200).json({ data: jsonResult }).end();
 });
 
 module.exports = router;
