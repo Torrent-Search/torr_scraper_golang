3037440a4ce16a50c0b9f502c1a0e5a5fe494c02
Tejas_patil <tejasvp25@gmail.com>
2020-05-01 14:34:26 +0530

    1337x.js: Remove unused import

--- scrapers/1337x.js
+++ scrapers/1337x.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const BASE_URL = require("./constants").BASE_URL_1337X;
