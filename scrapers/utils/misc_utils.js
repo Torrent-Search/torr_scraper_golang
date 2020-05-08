edccfc237233934a365f119de7c80d9e2d766e49
Tejas_patil <tejasvp25@gmail.com>
2020-05-08 15:14:35 +0530

    Format code

--- scrapers/utils/misc_utils.js
+++ scrapers/utils/misc_utils.js
@@ -1,6 +1,8 @@
-function isMagnet(str) {	
-    if(str == undefined){str=""}
-    return str.match(/magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32}/i);	
+function isMagnet(str) {
+    if (str == undefined) {
+        str = "";
+    }
+    return str.match(/magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32}/i);
 }
 
-module.exports={isMagnet}
+module.exports = { isMagnet };
